222a09c561572c26dede317ffcca9a186b2e3dc9
aikonkali <brianzion63@gmail.com>
2022-12-08 13:33:26 +0300

    homescreen

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:notes_app/styles/app_style.dart';
 
+import '../widgets/note_card.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -14,56 +16,64 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppStyle.mainColor,
-      appBar: AppBar(
-        elevation: 0.0,
-        title: const Text('NOTES'),
-        centerTitle: true,
         backgroundColor: AppStyle.mainColor,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Your Recent Text:',
-              style: GoogleFonts.roboto(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 22,
+        appBar: AppBar(
+          elevation: 0.0,
+          title: const Text('NOTES'),
+          centerTitle: true,
+          backgroundColor: AppStyle.mainColor,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Your Recent Text:',
+                style: GoogleFonts.roboto(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Builder(builder: (context) {
-              return StreamBuilder<QuerySnapshot>(
-                stream:
-                    FirebaseFirestore.instance.collection('Notes').snapshots(),
-                builder: (context, AsyncSnapshot snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  if (snapshot.hasData) {
-                    return GridView(
+              const SizedBox(
+                height: 20.0,
+              ),
+              // Builder(builder: (context) {
+              //   return
+              Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('Notes')
+                      .snapshots(),
+                  builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    if (snapshot.hasData) {
+                      return GridView(
                         gridDelegate:
                             const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 2));
-                  }
-                  return Text(
-                    "There's No Notes",
-                    style: GoogleFonts.nunito(color: Colors.white),
-                  );
-                },
-              );
-            })
-          ],
-        ),
-      ),
-    );
+                                crossAxisCount: 2),
+                        children: snapshot.data!.docs
+                            .map((note) => noteCard(() {}, note))
+                            .toList(),
+                      );
+                    }
+                    return Text(
+                      "There's No Notes",
+                      style: GoogleFonts.nunito(color: Colors.white),
+                    );
+                  },
+
+                  // })
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
